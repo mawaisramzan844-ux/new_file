2c6c7cb92c1190f07a2a4af4b09fc45d0d426cbd
mawaisramzan844-ux <mawaisramzan844@gmail.com>
2026-01-18 14:05:03 +0500

    add new files

---
+++ float_example.py
@@ -0,0 +1,15 @@
+"""
+File Name: float_example.py
+Description:
+This script explains Float data type in Python with an example.
+"""
+
+# -----------------------------
+# Float Data Type Example
+# -----------------------------
+
+# Variable to store product price
+product_price = 149.75
+
+print("Product Price:", product_price)
+print("Data Type:", type(product_price))
---
+++ string_example.py
@@ -0,0 +1,15 @@
+"""
+File Name: string_example.py
+Description:
+This program demonstrates String data type in Python.
+"""
+
+# -----------------------------
+# String Data Type Example
+# -----------------------------
+
+# Variable to store user name
+user_name = "Awais"
+
+print("User Name:", user_name)
+print("Data Type:", type(user_name))
